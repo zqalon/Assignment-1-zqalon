d2d67c09fd23d5705d3548f3061ba2d5e0ebe325
zqalon <89801024+zqalon@users.noreply.github.com>
2022-10-17 18:09:12 -0700

    Update A1.java

--- A1.java
+++ A1.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.util.List;
 
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 
 
 public class A1 {
@@ -12,24 +11,13 @@ public class A1 {
 	public static void main(String args[]) throws Exception{
 		
 		try { 
-	        // Create an object of file reader 
-	        // class with CSV file as a parameter. 
 	        FileReader filereader = new FileReader(args[0]); 
 	  
-	        // create csvReader object and skip first Line 
 	        CSVReader csvReader = new CSVReader(filereader);
 	        List<String[]> allData = csvReader.readAll(); 
-	  
-	        // print Data 
-	        for (String[] row : allData) { 
-	            for (String cell : row) { 
-	                System.out.print(cell + "\t"); 
-	            } 
-	            System.out.println(); 
-	        } 
 	    } 
 	    catch (Exception e) { 
-	        e.printStackTrace(); 
+	        throw new Exception(e);
 	    } 
 	}
 	
